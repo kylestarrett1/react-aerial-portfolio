60ff63b87891ae13ec6f48a4bd74d970c218aa2d
kylestarrett1 <kylestarrett1@gmail.com>
2023-11-18 22:38:02 -0600

    Started to implement React Spring for contact page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Fragment, useRef, useEffect } from 'react';
 import './styles/sass/main.scss';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faGithub,
-  faLinkedin,
-  faCodepen,
-} from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import {
+//   faGithub,
+//   faLinkedin,
+//   faCodepen,
+// } from '@fortawesome/free-brands-svg-icons';
 
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
@@ -33,60 +33,72 @@ const App = () => {
   return (
     <Fragment>
       <Parallax pages={2} ref={ref}>
-        <ParallaxLayer offset={0} speed={1} factor={2}>
+        <ParallaxLayer offset={0} speed={1} factor={1}>
           <div id="wrapper">
             <div id="bg"></div>
             <div id="overlay"></div>
-            <div id="main">
-              {/* Header */}
-              <header id="header">
-                <h1>Kyle Starrett</h1>
-                <p>Full-Stack Developer</p>
-                <nav>
-                  <ul>
-                    <li>
-                      <a
-                        href="https://www.github.com/kylestarrett1"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="icon brands fa-github"
-                      >
-                        <FontAwesomeIcon icon={faGithub} />
-                        <span className="label">Github</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://www.linkedin.com/in/kylestarrett/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="icon brands fa-linkedin"
-                      >
-                        <FontAwesomeIcon icon={faLinkedin} />
-                        <span className="label">LinkedIn</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="https://codepen.io/kylestarrett1"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="icon brands fa-codepen"
-                      >
-                        <FontAwesomeIcon icon={faCodepen} />
-                        <span className="label">Codepen</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </header>
-            </div>
           </div>
         </ParallaxLayer>
 
-        <ParallaxLayer>
+        <ParallaxLayer sticky={{ start: 0, end: 0.3 }} speed={1}>
+          <div id="main">
+            {/* Header */}
+            <header id="header">
+              <h1>Kyle Starrett</h1>
+              <p>Full-Stack Developer</p>
+              <nav>
+                <ul>
+                  <li>
+                    <a
+                      href="https://www.github.com/kylestarrett1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="icon brands fa-github"
+                    >
+                      {/* <FontAwesomeIcon icon={faGithub} /> */}
+                      <span className="label">Github</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.linkedin.com/in/kylestarrett/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="icon brands fa-linkedin"
+                    >
+                      {/* <FontAwesomeIcon icon={faLinkedin} /> */}
+                      <span className="label">LinkedIn</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://codepen.io/kylestarrett1"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="icon brands fa-codepen"
+                    >
+                      {/* <FontAwesomeIcon icon={faCodepen} /> */}
+                      <span className="label">Codepen</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </header>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} factor={0.25} speed={1}>
           {/* Footer */}
-          <footer></footer>
+          <footer id="footer">
+            <form action="" className="contact">
+              <label htmlFor="name"></label>
+              <input type="text" className="input name" />
+              <label htmlFor="email"></label>
+              <input type="email" className="input email" />
+              <label htmlFor="message"></label>
+              <input type="message" className="input message" />
+            </form>
+          </footer>
         </ParallaxLayer>
       </Parallax>
     </Fragment>
