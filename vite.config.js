03d68412710966f7b6e07efb8e791ca2a18ca13b
kylestarrett1 <kylestarrett1@gmail.com>
2023-11-25 19:05:22 -0600

    Added packages to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/react-aerial-portfolio/',
+  plugins: [react(), viteCompression()],
+});
