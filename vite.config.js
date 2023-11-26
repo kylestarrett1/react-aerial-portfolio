b9ed6071d81a52d58a93c98e38b0fc28a6985f21
kylestarrett1 <kylestarrett1@gmail.com>
2023-11-25 20:05:10 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-aerial-portfolio/',
   plugins: [react(), viteCompression()],
 });
