0b801d82e3eab2e6da4669a9e79bfd8494efd366
kylestarrett1 <kylestarrett1@gmail.com>
2024-05-03 10:57:20 -0500

    Added a gallery to display projects with links. Implemented the React Spring framework to transition between pages. Added a button to switch to gallery from main page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { Fragment, useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+
 import copy from 'copy-to-clipboard';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faCaretDown } from '@fortawesome/free-solid-svg-icons';
+
+import Gallery from './gallery/Gallery';
+
 import './styles/css/main.css';
 
 const App = () => {
@@ -10,6 +15,8 @@ const App = () => {
   const [isError, setIsError] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
   const [showClipboardAnimation, setShowClipboardAnimation] = useState(false);
+
+  const ref = useRef();
 
   const writeToMobileClipboard = async () => {
     try {
@@ -101,79 +108,108 @@ const App = () => {
   }, [isError, isCopied]);
 
   return (
-    <Fragment>
-      <div id="wrapper">
-        <div id="bg"></div>
-        <div id="overlay"></div>
-        <div id="main">
-          <header id="header" tabIndex={0} aria-label="Portfolio">
-            <h1 aria-label="Portfolio of Kyle Starrett" tabIndex={0}>
-              Kyle Starrett
-            </h1>
-            <p aria-label="Job Description: Full-Stack Developer" tabIndex={0}>
-              Full-Stack Developer
-            </p>
-            <nav tabIndex={0} aria-label="Portfolio">
-              <ul>
-                <li>
-                  <a
-                    href="https://www.github.com/kylestarrett1"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="icon fa-icon brands"
-                    aria-label="Kyle Starrett's Github profile"
-                  >
-                    <FontAwesomeIcon
-                      className="fa-icon__child"
-                      icon={faGithub}
-                    />
-                    <span className="label">Github</span>
-                  </a>
-                </li>
+    <>
+      <Parallax id="container" pages={2} ref={ref}>
+        <ParallaxLayer speed={0}>
+          <div id="wrapper">
+            <div id="bg"></div>
+            <div id="overlay"></div>
+            <div id="main">
+              <header id="header" tabIndex={0} aria-label="Portfolio">
+                <h1 aria-label="Portfolio of Kyle Starrett" tabIndex={0}>
+                  Kyle Starrett
+                </h1>
+                <p
+                  aria-label="Job Description: Full-Stack Developer"
+                  tabIndex={0}
+                >
+                  Web Developer | Graphic Designer
+                </p>
+                <nav tabIndex={0} aria-label="Portfolio">
+                  <ul>
+                    <li>
+                      <a
+                        href="https://www.github.com/kylestarrett1"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="icon fa-icon brands"
+                        aria-label="Kyle Starrett's Github profile"
+                      >
+                        <FontAwesomeIcon
+                          className="fa-icon__child"
+                          icon={faGithub}
+                        />
+                        <span className="label">Github</span>
+                      </a>
+                    </li>
 
-                <li>
-                  <a
-                    href="https://www.linkedin.com/in/kylestarrett/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="icon fa-icon brands"
-                    aria-label="Kyle Starrett's LinkedIn profile"
-                  >
-                    <FontAwesomeIcon
-                      className="fa-icon__child"
-                      icon={faLinkedin}
-                    />
-                    <span className="label">LinkedIn</span>
-                  </a>
-                </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/in/kylestarrett/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="icon fa-icon brands"
+                        aria-label="Kyle Starrett's LinkedIn profile"
+                      >
+                        <FontAwesomeIcon
+                          className="fa-icon__child"
+                          icon={faLinkedin}
+                        />
+                        <span className="label">LinkedIn</span>
+                      </a>
+                    </li>
 
-                <li onTouchStart={handleTouchStart} onClick={handleEmailClick}>
-                  <a
-                    href="#!"
-                    className="icon fa-icon solid"
-                    aria-label="Email: kylestarrett1@gmail.com. Use enter to copy to clipboard."
-                  >
-                    <FontAwesomeIcon
-                      className="fa-icon__child"
-                      icon={faEnvelope}
-                    />
-                    <span className="label">Email</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </header>
-          <footer id="footer"></footer>
-        </div>
-        {!isError && isCopied ? (
-          <p className={`clipboard ${showClipboardAnimation ? 'animate' : ''}`}>
-            {copiedText}
-          </p>
-        ) : (
-          ''
-        )}
-      </div>
-    </Fragment>
+                    <li
+                      onTouchStart={handleTouchStart}
+                      onClick={handleEmailClick}
+                    >
+                      <a
+                        href="#!"
+                        className="icon fa-icon solid"
+                        aria-label="Email: kylestarrett1@gmail.com. Use enter to copy to clipboard."
+                      >
+                        <FontAwesomeIcon
+                          className="fa-icon__child"
+                          icon={faEnvelope}
+                        />
+                        <span className="label">Email</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </header>
+              <footer id="footer"></footer>
+            </div>
+            {!isError && isCopied ? (
+              <p
+                className={`clipboard ${
+                  showClipboardAnimation ? 'animate' : ''
+                }`}
+              >
+                {copiedText}
+              </p>
+            ) : (
+              ''
+            )}
+
+            <a
+              onClick={() => ref.current.scrollTo(1)}
+              className="icon gallery-link"
+              aria-label="A down arrow that links to the gallery."
+            >
+              <FontAwesomeIcon
+                className="gallery-link-icon"
+                icon={faCaretDown}
+              />
+            </a>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={1}>
+          <Gallery />
+        </ParallaxLayer>
+      </Parallax>
+    </>
   );
 };
 
---
+++ src/gallery/Gallery.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+
+import Natours from '../assets/images/Natours.png';
+import Omni from '../assets/images/Omni-Consumer-Products.png';
+import WebRTC from '../assets/images/WebRTC.png';
+
+const Gallery = () => {
+  const [mouseDownAt, setMouseDownAt] = useState(0);
+  const [prevPercentage, setPrevPercentage] = useState(0);
+  const [percentage, setPercentage] = useState(0);
+
+  const handleOnDown = (e) => setMouseDownAt(e.clientX);
+
+  const handleOnUp = () => {
+    setMouseDownAt(0);
+    setPrevPercentage(percentage);
+  };
+
+  const handleOnMove = (e) => {
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - e.clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    const nextPercentageUnconstrained =
+      prevPercentage + (mouseDelta / maxDelta) * -100;
+    const nextPercentage = Math.max(
+      Math.min(nextPercentageUnconstrained, 0),
+      -100
+    );
+
+    setPercentage(nextPercentage);
+
+    const track = document.getElementById('image-track');
+    track.style.transform = `translate(${nextPercentage}%, -50%)`;
+
+    const images = track.getElementsByClassName('image');
+    for (const image of images) {
+      image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+  };
+
+  // Attach event listeners
+  React.useEffect(() => {
+    window.addEventListener('mousedown', handleOnDown);
+    window.addEventListener('touchstart', handleOnDown);
+    window.addEventListener('mouseup', handleOnUp);
+    window.addEventListener('touchend', handleOnUp);
+    window.addEventListener('mousemove', handleOnMove);
+    window.addEventListener('touchmove', handleOnMove);
+
+    return () => {
+      window.removeEventListener('mousedown', handleOnDown);
+      window.removeEventListener('touchstart', handleOnDown);
+      window.removeEventListener('mouseup', handleOnUp);
+      window.removeEventListener('touchend', handleOnUp);
+      window.removeEventListener('mousemove', handleOnMove);
+      window.removeEventListener('touchmove', handleOnMove);
+    };
+  });
+
+  return (
+    <div id="gallery-main">
+      <div className="gallery-container">
+        <div
+          id="image-track"
+          data-mouse-down-at="0"
+          data-preview-percentage="0"
+        >
+          <div className="imgContainer">
+            <a
+              href="https://starrett-kyle-natours.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>Natours</h2>
+            </a>
+            <img className="image" src={Natours} draggable="false" />
+          </div>
+
+          <div className="imgContainer">
+            <a
+              href="https://starrett-omni-consumer-products.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>Omni Consumer Products</h2>
+            </a>
+            <img className="image" src={Omni} draggable="false" />
+          </div>
+
+          <div className="imgContainer">
+            <a
+              href="https://github.com/kylestarrett1/WebRTC-RT-Chat"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h2>WebRTC Chat</h2>
+            </a>
+            <img className="image" src={WebRTC} draggable="false" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
